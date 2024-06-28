408fb4b726f5af702c776703181912641f4811f8
Vatsal Shah <svatsal64@gmail.com>
2024-06-28 20:52:52 +0400

    fixed styling of bodyPart cards

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -8,14 +8,30 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
     alignItems="center"
     justifyContent="center"
     className="bodyPart-card"
-    sx={bodyPart === item ? { borderTop: '4px solid #FF2625', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
+    sx={{
+      borderTop: bodyPart === item ? '4px solid #FF2625' : '1px solid black',
+      borderRight: '1px solid black',
+      borderBottom: '1px solid black',
+      borderLeft: '1px solid black',
+      background: '#fff',
+      borderRadius: '0 0 20px 20px',
+      width: '270px',
+      height: '282px',
+      cursor: 'pointer',
+      gap: '47px',
+      '&:hover': {
+        borderTop: '4px solid #FF2625',
+      }
+    }}
     onClick={() => {
       setBodyPart(item);
       window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
     }}
   >
     <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
-    <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
+    <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize">
+      {item}
+    </Typography>
   </Stack>
 );
 
