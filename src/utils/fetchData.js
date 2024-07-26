3f5e10b6454460ec1bbd6a4782f27bcfd881a4e5
Vatsal Shah <svatsal64@gmail.com>
2024-07-27 01:04:43 +0400

    updated api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,12 @@
 export const exerciseOptions = {
   method: 'GET',
-  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back?limit=100&offset=0',
-  headers: {
-    'x-rapidapi-key': 'f324d57ac4msh604dbd696ecc0b4p1946bfjsnb4eaaf57ebde',
-    'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
-  }
+	hostname: 'exercisedb.p.rapidapi.com',
+	port: null,
+	path: '/exercises/bodyPart/back?limit=10&offset=0',
+	headers: {
+		'x-rapidapi-key': '07604365e0msh9c6daec8d946547p16d05ajsn3c440989a621',
+		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+	}
 };
 
 export const youtubeOptions = {
